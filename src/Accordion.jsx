9d245fa2a81fe3c10e68e1d50805454c21b4be18
Ellie <112726692+elliezub@users.noreply.github.com>
2023-08-31 14:18:27 -0400

    update

---
+++ src/Accordion.jsx
@@ -0,0 +1,212 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div className="max-w-xs m-8 mb-12 lg:max-w-sm lg:mr-24 lg:mb-24 lg:ml-64">
+      <Accordion
+        className="rounded-t-lg"
+        sx={{ border: "0px" }}
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white",
+            borderBottom: "1px solid #e5e7eb",
+            padding: "0",
+          }}
+          aria-controls="panel1d-content"
+          id="panel1d-header"
+        >
+          <Typography className="question"  sx={{
+        fontWeight: expanded === "panel1" ? 'bold' : 'normal',
+        color: expanded === "panel1" ? '#1d1e35' : '#4a4b5e',
+        transition: 'color 0.3s',
+        '&:hover': expanded !== "panel1" ? { color: '#f47c57' } : {}
+      }}>How many team members can I invite?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx= {{
+            color: "#787887",
+          }}>
+            You can invite up to 2 additional users on the Free plan. There is
+            no limit on team members for the Premium plan.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        expanded={expanded === "panel2"}
+        sx={{ border: "0px" }}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white",
+            borderBottom: "1px solid #e5e7eb",
+            padding: "0",
+          }}
+          aria-controls="panel2d-content"
+          id="panel2d-header"
+        >
+          <Typography className="question"  sx={{
+        fontWeight: expanded === "panel2" ? 'bold' : 'normal',
+        color: expanded === "panel2" ? '#1d1e35' : '#4a4b5e',
+        transition: 'color 0.3s',
+        '&:hover': expanded !== "panel2" ? { color: '#f47c57' } : {}
+      }}>What is the maximum file upload size?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx= {{
+            color: "#787887",
+          }}>
+            No more than 2GB. All files in your account must fit your allotted
+            storage space.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        expanded={expanded === "panel3"}
+        sx={{ border: "0px" }}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white",
+            borderBottom: "1px solid #e5e7eb",
+            padding: "0",
+          }}
+          aria-controls="panel3d-content"
+          id="panel3d-header"
+        >
+          <Typography className="question"  sx={{
+        fontWeight: expanded === "panel3" ? 'bold' : 'normal',
+        color: expanded === "panel3" ? '#1d1e35' : '#4a4b5e',
+        transition: 'color 0.3s',
+        '&:hover': expanded !== "panel3" ? { color: '#f47c57' } : {}
+      }}>How do I reset my password?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx= {{
+            color: "#787887",
+          }}>
+            Click “Forgot password” from the login page or “Change password”
+            from your profile page. A reset link will be emailed to you.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        expanded={expanded === "panel4"}
+        sx={{ border: "0px" }}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white",
+            borderBottom: "1px solid #e5e7eb",
+            padding: "0",
+          }}
+          aria-controls="panel4d-content"
+          id="panel4d-header"
+        >
+          <Typography className="question"  sx={{
+        fontWeight: expanded === "panel4" ? 'bold' : 'normal',
+        color: expanded === "panel4" ? '#1d1e35' : '#4a4b5e',
+        transition: 'color 0.3s',
+        '&:hover': expanded !== "panel4" ? { color: '#f47c57' } : {}
+      }}>Can I cancel my subscription?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx= {{
+            color: "#787887",
+          }}>
+            Yes! Send us a message and we’ll process your request no questions
+            asked.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion
+        className="rounded-b-lg"
+        sx={{ border: "0px" }}
+        expanded={expanded === "panel5"}
+        onChange={handleChange("panel5")}
+      >
+        <AccordionSummary
+          sx={{
+            backgroundColor: "white",
+            borderBottom: "1px solid #e5e7eb",
+            padding: "0",
+          }}
+          aria-controls="panel5d-content"
+          id="panel5d-header"
+        >
+          <Typography className="question" sx={{
+        fontWeight: expanded === "panel5" ? 'bold' : 'normal',
+        color: expanded === "panel5" ? '#1d1e35' : '#4a4b5e',
+        transition: 'color 0.3s',
+        '&:hover': expanded !== "panel5" ? { color: '#f47c57' } : {}
+      }}>Do you provide additional support?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography sx= {{
+            color: "#787887",
+          }}>
+            Chat and email support is available 24/7. Phone lines are open
+            during normal business hours.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import Accordion from "./Accordion";
+import "./App.css";
+import BgPattern from "./assets/bg-pattern-desktop.svg";
+import MobileBg from "./assets/bg-pattern-mobile.svg";
+import Box from "./assets/illustration-box-desktop.svg";
+import WomanDesktop from "./assets/illustration-woman-online-desktop.svg";
+import WomanMobile from "./assets/illustration-woman-online-mobile.svg";
+
+function App() {
+  return (
+    <>
+      <body className="bg-gradient-to-b from-[#af67e9] to-[#6565e7] flex justify-center items-center min-h-screen">
+        <div className="relative mt-16 lg:mt-6">
+          <img src={Box} alt="cube" className="absolute cube hidden lg:block" />
+          <div className="bg-white flex w-full h-full text-[#181028] shadow-lg rounded-3xl mx-2 relative lg:overflow-hidden">
+            {/* desktop */}
+            <div className="w-72 mr-2 hidden lg:block">
+              <img
+                src={BgPattern}
+                alt="pattern"
+                className="bgpattern absolute"
+              />
+              <img
+                src={WomanDesktop}
+                alt="Woman looking at a computer"
+                className="womancomputer absolute"
+              />
+            </div>
+
+            <div className="hidden lg:block">
+              <h2 className="font-bold text-4xl mb-8 ml-12 mt-20 lg:ml-64">
+                FAQ
+              </h2>
+              <Accordion TransitionProps={{ unmountOnExit: true }} />
+            </div>
+            {/* mobile */}
+            <div className="block lg:hidden relative">
+              <img
+                src={MobileBg}
+                alt="mobile pattern"
+                className="absolute mobilebg mx-12"
+              />
+              <img
+                src={WomanMobile}
+                alt="Woman looking at a computer"
+                className="absolute -top-44 m-12"
+              />
+              <h2 className="font-bold text-4xl text-center mb-2 mt-40">FAQ</h2>
+              <Accordion TransitionProps={{ unmountOnExit: true }} />
+            </div>
+          </div>
+        </div>
+      </body>
+    </>
+  );
+}
+
+export default App;
